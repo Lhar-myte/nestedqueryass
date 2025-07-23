4ef7b6a3c92558544c015606500b41f6f670df99
Lhar-myte <lharmyte@gmail.com>
2025-07-23 14:06:57 +0100

    Add files via upload

---
+++ nestedqueryass.sql
@@ -0,0 +1,13 @@
+-- find the percentage of employee whose marital status is divorced in the organisation
+SELECT *
+FROM employee_attrition;
+
+SELECT
+  ROUND(
+    (SELECT COUNT(maritalstatus) FROM employee_attrition WHERE maritalstatus = 'Divorced') * 100.0 /
+    (SELECT COUNT(maritalstatus) FROM employee_attrition),
+    2
+  ) AS Divorced_Percentage;
+
+
+	
